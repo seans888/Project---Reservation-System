9f1d29acf9f9eb8481b732e68c1719b67a5ff9f1
Han Reyes <hannahreyes54@gmail.com>
2016-08-09 08:32:57 +0800

    configure

--- Application/advanced/backend/config/main-local.php
+++ Application/advanced/backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '_31fmp6llnsTS_-EnqfOYERGOglQYE1t',
+            'cookieValidationKey' => '8jsspukKuKzXyw18eqj9NOpMa2-q047n',
         ],
     ],
 ];
