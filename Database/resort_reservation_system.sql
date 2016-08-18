0a4f2a4d3d1a1fb50d6deb504021451044682618
Han Reyes <hannahreyes54@gmail.com>
2016-08-18 23:54:22 +0800

    updated database

--- Database/resort_reservation_system.sql
+++ Database/resort_reservation_system.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2016 at 11:52 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.6
+-- Generation Time: Aug 18, 2016 at 05:52 PM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,47 @@ SET time_zone = "+00:00";
 --
 -- Database: `resort_reservation_system`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `backend`
+--
+
+CREATE TABLE `backend` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `frontend`
+--
+
+CREATE TABLE `frontend` (
+  `id` int(15) NOT NULL,
+  `last_name` varchar(60) NOT NULL,
+  `first_name` varchar(60) NOT NULL,
+  `middle_name` int(60) DEFAULT NULL,
+  `address` int(80) NOT NULL,
+  `contact_number` varchar(20) NOT NULL,
+  `check_in` date NOT NULL,
+  `check_out` date NOT NULL,
+  `room_type` int(60) NOT NULL,
+  `room_number` varchar(20) NOT NULL,
+  `number_of_kids` int(10) DEFAULT NULL,
+  `number_of_adults` int(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `requests` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -62,6 +103,21 @@ CREATE TABLE `user` (
 --
 
 --
+-- Indexes for table `backend`
+--
+ALTER TABLE `backend`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
+
+--
+-- Indexes for table `frontend`
+--
+ALTER TABLE `frontend`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `migration`
 --
 ALTER TABLE `migration`
@@ -80,6 +136,16 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `backend`
+--
+ALTER TABLE `backend`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `frontend`
+--
+ALTER TABLE `frontend`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
