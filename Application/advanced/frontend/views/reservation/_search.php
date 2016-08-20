7c532adb2e16e57af97aeed600621c5c82a8aad0
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 01:05:27 +0800

    frontend crud reservation

--- Application/advanced/frontend/views/reservation/_search.php
+++ Application/advanced/frontend/views/reservation/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model backend\models\ReservationSearch */
+/* @var $model frontend\models\ReservationSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
