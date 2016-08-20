bbab45bee9d810b93b3c70d81a86aec39ab23fa3
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 00:12:41 +0800

    generate

---
+++ Application/advanced/common/models/Regular.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "regular".
+ *
+ * @property integer $id
+ * @property string $salary
+ * @property string $status
+ * @property string $classification
+ * @property string $nature_of_service
+ *
+ * @property Employee[] $employees
+ */
+class Regular extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'regular';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id'], 'integer'],
+            [['salary'], 'number'],
+            [['status'], 'string', 'max' => 50],
+            [['classification'], 'string', 'max' => 45],
+            [['nature_of_service'], 'string', 'max' => 30],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'salary' => 'Salary',
+            'status' => 'Status',
+            'classification' => 'Classification',
+            'nature_of_service' => 'Nature Of Service',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEmployees()
+    {
+        return $this->hasMany(Employee::className(), ['regular_id' => 'id']);
+    }
+}
