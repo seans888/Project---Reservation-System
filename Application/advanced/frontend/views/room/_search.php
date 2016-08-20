1f4eb807d5810ddb72ee9830be60bdf3c64eacb5
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 01:06:38 +0800

    backend crud room

--- Application/advanced/frontend/views/room/_search.php
+++ Application/advanced/frontend/views/room/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model frontend\models\RoomSearch */
+/* @var $model backend\models\RoomSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
