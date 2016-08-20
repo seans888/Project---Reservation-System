a462d7454284a7d69c4595dcf01c243cd7d86127
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 00:10:36 +0800

    generate

---
+++ Application/advanced/common/models/Manager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "manager".
+ *
+ * @property integer $id
+ * @property integer $admin_id
+ * @property string $username
+ * @property string $access_date
+ *
+ * @property Employee[] $employees
+ */
+class Manager extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'manager';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id', 'admin_id'], 'integer'],
+            [['access_date'], 'safe'],
+            [['username'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'admin_id' => 'Admin ID',
+            'username' => 'Username',
+            'access_date' => 'Access Date',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEmployees()
+    {
+        return $this->hasMany(Employee::className(), ['manager_id' => 'id']);
+    }
+}
