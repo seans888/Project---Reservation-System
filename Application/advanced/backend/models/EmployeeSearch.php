eb67cf9ebe7662ae8743a9be3d340b4399832de0
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 00:56:41 +0800

    backend crud Employee

---
+++ Application/advanced/backend/models/EmployeeSearch.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Employee;
+
+/**
+ * EmployeeSearch represents the model behind the search form about `common\models\Employee`.
+ */
+class EmployeeSearch extends Employee
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'manager_id', 'regular_id'], 'integer'],
+            [['name', 'contact_number', 'rate', 'hired_date', 'department', 'type', 'position', 'email', 'address', 'birthday'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Employee::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'hired_date' => $this->hired_date,
+            'birthday' => $this->birthday,
+            'manager_id' => $this->manager_id,
+            'regular_id' => $this->regular_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'contact_number', $this->contact_number])
+            ->andFilterWhere(['like', 'rate', $this->rate])
+            ->andFilterWhere(['like', 'department', $this->department])
+            ->andFilterWhere(['like', 'type', $this->type])
+            ->andFilterWhere(['like', 'position', $this->position])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'address', $this->address]);
+
+        return $dataProvider;
+    }
+}
---
+++ Application/advanced/frontend/views/employee/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\EmployeeSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Employees';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="employee-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Employee', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'contact_number',
+            'rate',
+            'hired_date',
+            // 'department',
+            // 'type',
+            // 'position',
+            // 'email:email',
+            // 'address',
+            // 'birthday',
+            // 'manager_id',
+            // 'regular_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
