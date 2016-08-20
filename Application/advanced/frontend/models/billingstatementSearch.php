b732be067bd3cb3bd1a456cd6278d7c71cc13b37
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 00:44:27 +0800

    generate crud - backend billing statement

---
+++ Application/advanced/frontend/models/billingstatementSearch.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\billingstatement;
+
+/**
+ * billingstatementSearch represents the model behind the search form about `common\models\billingstatement`.
+ */
+class billingstatementSearch extends billingstatement
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'number', 'employee_id', 'customer_id'], 'integer'],
+            [['mode_of_payment', 'date', 'status'], 'safe'],
+            [['amount'], 'number'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = billingstatement::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'amount' => $this->amount,
+            'number' => $this->number,
+            'date' => $this->date,
+            'employee_id' => $this->employee_id,
+            'customer_id' => $this->customer_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'mode_of_payment', $this->mode_of_payment])
+            ->andFilterWhere(['like', 'status', $this->status]);
+
+        return $dataProvider;
+    }
+}
--- Application/advanced/frontend/views/billing-statement/index.php
+++ Application/advanced/frontend/views/billing-statement/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel backend\models\billingstatementSearch */
+/* @var $searchModel frontend\models\billingstatementSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Billingstatements';
