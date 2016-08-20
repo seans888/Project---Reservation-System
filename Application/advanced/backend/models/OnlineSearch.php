6ed1f501a7aa35d1951273d48a1a58cc39e86e52
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 00:59:49 +0800

    backend crud online

---
+++ Application/advanced/backend/models/OnlineSearch.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Online;
+
+/**
+ * OnlineSearch represents the model behind the search form about `common\models\Online`.
+ */
+class OnlineSearch extends Online
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['actual_access_date', 'actual_reserve_date'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Online::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'actual_access_date' => $this->actual_access_date,
+            'actual_reserve_date' => $this->actual_reserve_date,
+        ]);
+
+        return $dataProvider;
+    }
+}
---
+++ Application/advanced/frontend/views/online/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Online */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="online-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'actual_access_date')->textInput() ?>
+
+    <?= $form->field($model, 'actual_reserve_date')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
