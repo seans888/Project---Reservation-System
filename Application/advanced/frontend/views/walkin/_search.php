ccd05489382a791194ddef41a8449526c5c708f9
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 01:09:54 +0800

    backend crud walkin

--- Application/advanced/frontend/views/walkin/_search.php
+++ Application/advanced/frontend/views/walkin/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model frontend\models\WalkinSearch */
+/* @var $model backend\models\WalkinSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
