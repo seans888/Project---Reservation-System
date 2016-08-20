2df39864fa501aa3b7bc19857e5186a78263e165
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 01:06:14 +0800

    front end crud room

---
+++ Application/advanced/frontend/views/room/_search.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\RoomSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="room-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'description') ?>
+
+    <?= $form->field($model, 'type') ?>
+
+    <?= $form->field($model, 'rate') ?>
+
+    <?= $form->field($model, 'capacity') ?>
+
+    <?php // echo $form->field($model, 'availability') ?>
+
+    <?php // echo $form->field($model, 'billing statement_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
