e7c5548bda2f44994f5c5925a71b2ff3ecd182d2
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 01:01:52 +0800

    back end crud regular

--- Application/advanced/frontend/views/regular/_search.php
+++ Application/advanced/frontend/views/regular/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model frontend\models\RegularSearch */
+/* @var $model backend\models\RegularSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
--- Application/advanced/frontend/views/regular/index.php
+++ Application/advanced/frontend/views/regular/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel frontend\models\RegularSearch */
+/* @var $searchModel backend\models\RegularSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Regulars';
