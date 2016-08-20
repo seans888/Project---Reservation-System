6ff6a0b3248b83fbdca6bc7866ea90e3548a6603
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 01:46:21 +0800

    overwrite

--- Application/advanced/frontend/controllers/ServiceController.php
+++ Application/advanced/frontend/controllers/ServiceController.php
@@ -4,7 +4,7 @@ namespace frontend\controllers;
 
 use Yii;
 use common\models\Service;
-use frontend\models\ServiceSearch;
+use common\models\ServiceSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
--- Application/advanced/frontend/views/service/_search.php
+++ Application/advanced/frontend/views/service/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model frontend\models\ServiceSearch */
+/* @var $model common\models\ServiceSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
