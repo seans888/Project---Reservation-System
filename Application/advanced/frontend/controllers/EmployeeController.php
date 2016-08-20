253e6fb2c066ae20e81831f67c7781f2adf22b9b
Han Reyes <hannahreyes54@gmail.com>
2016-08-21 00:57:15 +0800

    front end crud employee

---
+++ Application/advanced/frontend/controllers/EmployeeController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\Employee;
+use frontend\models\EmployeeSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * EmployeeController implements the CRUD actions for Employee model.
+ */
+class EmployeeController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Employee models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new EmployeeSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Employee model.
+     * @param integer $id
+     * @param integer $manager_id
+     * @param integer $regular_id
+     * @return mixed
+     */
+    public function actionView($id, $manager_id, $regular_id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id, $manager_id, $regular_id),
+        ]);
+    }
+
+    /**
+     * Creates a new Employee model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Employee();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id, 'manager_id' => $model->manager_id, 'regular_id' => $model->regular_id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Employee model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @param integer $manager_id
+     * @param integer $regular_id
+     * @return mixed
+     */
+    public function actionUpdate($id, $manager_id, $regular_id)
+    {
+        $model = $this->findModel($id, $manager_id, $regular_id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id, 'manager_id' => $model->manager_id, 'regular_id' => $model->regular_id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Employee model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @param integer $manager_id
+     * @param integer $regular_id
+     * @return mixed
+     */
+    public function actionDelete($id, $manager_id, $regular_id)
+    {
+        $this->findModel($id, $manager_id, $regular_id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Employee model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @param integer $manager_id
+     * @param integer $regular_id
+     * @return Employee the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id, $manager_id, $regular_id)
+    {
+        if (($model = Employee::findOne(['id' => $id, 'manager_id' => $manager_id, 'regular_id' => $regular_id])) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
