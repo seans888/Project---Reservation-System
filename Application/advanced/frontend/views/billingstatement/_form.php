35f673fc3ce5d2df6c39b44fe37aff44c3cb676a
Maica Belchez <aciambelchez@gmail.com>
2016-08-21 11:17:32 +0800

    dropDownList

--- Application/advanced/frontend/views/billingstatement/_form.php
+++ Application/advanced/frontend/views/billingstatement/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\Employee;
+use common\models\Customer;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Billingstatement */
@@ -24,9 +27,25 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'employee_id')->textInput() ?>
+    <?= $form->field($model, 'employee_id')->dropDownList(
+		ArrayHelper::map( Employee::find()->all(), 'id', 'name' ),
+		[
+			'prompt'=>'Select Employee',
+			//'onchange'=>
+		    //	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+			//		$( "select#model-contact" ).html( data );
+			//	});'
+		]); ?>
 
-    <?= $form->field($model, 'customer_id')->textInput() ?>
+    <?= $form->field($model, 'customer_id')->dropDownList(
+		ArrayHelper::map( Customer::find()->all(), 'id', 'name' ),
+		[
+			'prompt'=>'Select Customer',
+			//'onchange'=>
+		    //	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+			//		$( "select#model-contact" ).html( data );
+			//	});'
+		]); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- Application/advanced/frontend/views/employee/_form.php
+++ Application/advanced/frontend/views/employee/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\Manager;
+use common\models\Regular;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Employee */
@@ -34,9 +37,25 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'birthday')->textInput() ?>
 
-    <?= $form->field($model, 'manager_id')->textInput() ?>
+    <?= $form->field($model, 'manager_id')->dropDownList(
+		ArrayHelper::map( Manager::find()->all(), 'id',  'username' ),
+		[
+			'prompt'=>'Select Manager',
+			//'onchange'=>
+		    //	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+			//		$( "select#model-contact" ).html( data );
+			//	});'
+		]); ?>
 
-    <?= $form->field($model, 'regular_id')->textInput() ?>
+    <?= $form->field($model, 'regular_id')->dropDownList(
+		ArrayHelper::map( Regular::find()->all(), 'id', 'status' ),
+		[
+			'prompt'=>'Select Regular',
+			//'onchange'=>
+		    //	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+			//		$( "select#model-contact" ).html( data );
+			//	});'
+		]); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
