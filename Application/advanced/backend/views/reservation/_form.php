e5972bda484dc8bf9e26540202fdbd77ecc31f6c
Maica Belchez <aciambelchez@gmail.com>
2016-08-23 02:06:15 +0800

    dropDownList - reservation (backend)

--- Application/advanced/backend/views/reservation/_form.php
+++ Application/advanced/backend/views/reservation/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\Walkin;
+use common\models\Online;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Reservation */
@@ -20,9 +23,27 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'receipt_number')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'online_id')->textInput() ?>
+    <?= $form->field($model, 'online_id')->dropDownList(
+		ArrayHelper::map( Online::find()->all(), 'id', 'actual_reserve_date' ),
+		[
+			'prompt'=>'Select Online',
+			//'onchange'=>
+			//	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function
+			//	( data ){
+			//		$( "select#model-contact" ).html( data );
+			//	});'
+		]); ?>
 
-    <?= $form->field($model, 'walkin_id')->textInput() ?>
+    <?= $form->field($model, 'walkin_id')->dropDownList(
+		ArrayHelper::map( Walkin::find()->all(), 'id', 'actual_time_reserve' ),
+		[
+			'prompt'=>'Select Walkin',
+			//'onchange'=>
+			//	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function
+			//	( data ){
+			//		$( "select#model-contact" ).html( data );
+			//	});'
+		]); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
