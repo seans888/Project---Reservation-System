49419ab77a2a52e3d3a57d3a3adab0d72d7099ba
Maica Belchez <aciambelchez@gmail.com>
2016-08-22 16:08:52 +0800

    add

--- Application/advanced/frontend/views/site/about.php
+++ Application/advanced/frontend/views/site/about.php
@@ -2,15 +2,70 @@
 
 /* @var $this yii\web\View */
 
-use yii\helpers\Html;
+/*use yii\helpers\Html;*/
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+    
+	<div class="jumbotron">
+	
+        <h1>About The Company</h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+        <img src = "http://freetriptip.com/wp-content/uploads/2014/06/Aruba-Collage2.jpg" style="width:100%">
+		
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+           dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+           ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+           fugiat nulla pariatur.</p>
+    </div>
+	
+	    <div class="body-content">
 
-    <code><?= __FILE__ ?></code>
+        <div class="row">
+            <div class="col-lg-4">
+			
+			<img src = "http://www.superbonesconference.com/eastsite/images/2015/SBSWEast/marriott-collage.jpg" style="width:100%">
+                <h2>History</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Read More &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+			
+			<img src = "http://www.traveltimes.com.mx/images/2015/Junio/Hoteles/collage_Mision.jpg" style="width:100%">
+                <h2>Team</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Read More &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+			
+			<img src = "http://image.shutterstock.com/z/stock-photo-hotel-collage-housekeeping-staff-at-work-158835149.jpg" style="width:100%">
+                <h2>FAQ</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Read More &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+    
+
+    
 </div>
--- Application/advanced/frontend/views/site/index.php
+++ Application/advanced/frontend/views/site/index.php
@@ -7,45 +7,51 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+	
+        <h1>CALIMBORACAY</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <img src = "http://freetriptip.com/wp-content/uploads/2014/06/Aruba-Collage2.jpg" style="width:100%">
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+			
+			<img src = "http://www.superbonesconference.com/eastsite/images/2015/SBSWEast/marriott-collage.jpg" style="width:100%">
+                <h2>Gallery</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Read More &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+			
+			<img src = "http://www.traveltimes.com.mx/images/2015/Junio/Hoteles/collage_Mision.jpg" style="width:100%">
+                <h2>Amenities</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Read More &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+			
+			<img src = "http://image.shutterstock.com/z/stock-photo-hotel-collage-housekeeping-staff-at-work-158835149.jpg" style="width:100%">
+                <h2>Services</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Read More &raquo;</a></p>
             </div>
         </div>
 
