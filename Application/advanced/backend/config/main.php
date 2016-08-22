62516f14f0e3e889a29dbf3daeef456b6cbf6ae4
Maica Belchez <aciambelchez@gmail.com>
2016-08-23 00:49:14 +0800

    admin

--- Application/advanced/backend/config/main.php
+++ Application/advanced/backend/config/main.php
@@ -25,7 +25,7 @@ return [
 			'savePath' => sys_get_temp_dir(),
 		],
 		'request' => [
-		    'cookieValidationKey' =>[eilfcflsHbkSvLpWGYbv],
+		    'cookieValidationKey' =>'[eilfcflsHbkSvLpWGYbv]',
 			'csrfParam' => '_backendCSRF',
 		],
         'log' => [
