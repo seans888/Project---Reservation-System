f64c2093c4c5d98aae79044b7d20a48a7445d48b
Maica Belchez <aciambelchez@gmail.com>
2016-08-22 21:17:46 +0800

    generate model and crud for billing (frontend)

--- Application/advanced/frontend/views/billingstatement/_form.php
+++ Application/advanced/frontend/views/billingstatement/_form.php
@@ -2,9 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\helpers\ArrayHelper;
-use common\models\Employee;
-use common\models\Customer;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Billingstatement */
@@ -14,8 +11,6 @@ use common\models\Customer;
 <div class="billingstatement-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'id')->textInput() ?>
 
     <?= $form->field($model, 'mode_of_payment')->textInput(['maxlength' => true]) ?>
 
@@ -27,25 +22,9 @@ use common\models\Customer;
 
     <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'employee_id')->dropDownList(
-		ArrayHelper::map( Employee::find()->all(), 'id', 'name' ),
-		[
-			'prompt'=>'Select Employee',
-			//'onchange'=>
-		    //	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
-			//		$( "select#model-contact" ).html( data );
-			//	});'
-		]); ?>
+    <?= $form->field($model, 'employee_id')->textInput() ?>
 
-    <?= $form->field($model, 'customer_id')->dropDownList(
-		ArrayHelper::map( Customer::find()->all(), 'id', 'name' ),
-		[
-			'prompt'=>'Select Customer',
-			//'onchange'=>
-		    //	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
-			//		$( "select#model-contact" ).html( data );
-			//	});'
-		]); ?>
+    <?= $form->field($model, 'customer_id')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
