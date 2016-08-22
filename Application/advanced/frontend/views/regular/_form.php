9af16754ae881fdaec8185959be4c188a5f61f8a
Maica Belchez <aciambelchez@gmail.com>
2016-08-22 21:04:22 +0800

    generate model and crud for regular (frontend)

--- Application/advanced/frontend/views/regular/_form.php
+++ Application/advanced/frontend/views/regular/_form.php
@@ -12,8 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
-
     <?= $form->field($model, 'salary')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
