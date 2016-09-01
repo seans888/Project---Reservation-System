deb8081f808a5e5110b383b211529a9f00b65d53
Maica Belchez <aciambelchez@gmail.com>
2016-09-02 02:22:17 +0800

    correcting the error
    there's still error

--- Application/advanced/common/models/Room.php
+++ Application/advanced/common/models/Room.php
@@ -8,15 +8,15 @@ use Yii;
  * This is the model class for table "room".
  *
  * @property integer $id
- * @property string $description
  * @property string $type
  * @property string $rate
  * @property integer $capacity
  * @property string $availability
- * @property integer $billingstatement_id
+ * @property integer $billing statement_id
  *
  * @property ReservationHasRoom[] $reservationHasRooms
- * @property Billingstatement $billingstatement
+ * @property Billingstatement $billingStatement
+ * @property Service[] $services
  */
 class Room extends \yii\db\ActiveRecord
 {
@@ -34,12 +34,11 @@ class Room extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['capacity', 'billingstatement_id'], 'integer'],
-            [['billingstatement_id'], 'required'],
-            [['description'], 'string', 'max' => 50],
+            [['capacity', 'billing statement_id'], 'integer'],
+            [['billing statement_id'], 'required'],
             [['type', 'availability'], 'string', 'max' => 45],
             [['rate'], 'string', 'max' => 20],
-            [['billingstatement_id'], 'exist', 'skipOnError' => true, 'targetClass' => Billingstatement::className(), 'targetAttribute' => ['billingstatement_id' => 'id']],
+            [['billing statement_id'], 'exist', 'skipOnError' => true, 'targetClass' => Billingstatement::className(), 'targetAttribute' => ['billing statement_id' => 'id']],
         ];
     }
 
@@ -50,12 +49,11 @@ class Room extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'description' => 'Description',
             'type' => 'Type',
             'rate' => 'Rate',
             'capacity' => 'Capacity',
             'availability' => 'Availability',
-            'billingstatement_id' => 'Billingstatement ID',
+            'billing statement_id' => 'Billing Statement ID',
         ];
     }
 
@@ -70,8 +68,16 @@ class Room extends \yii\db\ActiveRecord
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getBillingstatement()
+    public function getBillingStatement()
     {
-        return $this->hasOne(Billingstatement::className(), ['id' => 'billingstatement_id']);
+        return $this->hasOne(Billingstatement::className(), ['id' => 'billing statement_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getServices()
+    {
+        return $this->hasMany(Service::className(), ['room_id' => 'id']);
     }
 }
