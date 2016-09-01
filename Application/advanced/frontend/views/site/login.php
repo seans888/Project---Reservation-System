c8e0699f46d1f0a379e7c7d78edf14f666667f4a
Maica Belchez <aciambelchez@gmail.com>
2016-09-02 02:42:27 +0800

    login

--- Application/advanced/frontend/views/site/login.php
+++ Application/advanced/frontend/views/site/login.php
@@ -13,13 +13,7 @@ $this->title = 'Log In';
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+    <p>Please fill out the following fields <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
                 <?= $form->field($model, 'password')->passwordInput() ?>
 
@@ -31,7 +25,13 @@ $this->title = 'Log In';
 
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+                </div>to login:</p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+                
 
             <?php ActiveForm::end(); ?>
         </div>
