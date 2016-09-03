b094069fa6b38b30e4ce5ae3b7e64323cd0eaf94
Kyle Lee <kylevlee@outlook.com>
2016-09-04 02:18:22 +0800

    dropdownlist for reservation and room id in reservation-has-room and fixed billingstatement_id in room

--- Application/advanced/frontend/views/reservation-has-room/_form.php
+++ Application/advanced/frontend/views/reservation-has-room/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\Reservation;
+use common\models\Room;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\ReservationHasRoom */
@@ -16,9 +19,25 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'reservation_end_date')->textInput() ?>
 
-    <?= $form->field($model, 'reservation_id')->textInput() ?>
+    <?= $form->field($model, 'reservation_id')->dropDownList(
+        ArrayHelper::map( Reservation::find()->all(), 'id', 'reservation_date' ),
+        [
+            'prompt'=>'Select Reservation Date',
+            //'onchange'=>
+            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+            //      $( "select#model-contact" ).html( data );
+            //  });'
+        ]); ?> 
 
-    <?= $form->field($model, 'room_id')->textInput() ?>
+    <?= $form->field($model, 'room_id')->dropDownList(
+        ArrayHelper::map( Room::find()->all(), 'id', 'type' ),
+        [
+            'prompt'=>'Select Room Type',
+            //'onchange'=>
+            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+            //      $( "select#model-contact" ).html( data );
+            //  });'
+        ]); ?> 
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
