471cab679488706fd9fd107fc9eb20c5b362d389
Kyle Lee <kvlee14@gmail.com>
2016-09-03 20:30:12 +0800

    return changes

--- Application/advanced/frontend/views/service/_form.php
+++ Application/advanced/frontend/views/service/_form.php
@@ -2,11 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\helpers\ArrayHelper;
-use common\models\Employee;
-use common\models\Walkin;
-use common\models\Customer;
-use common\models\BillingStatement;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Service */
@@ -21,49 +16,11 @@ use common\models\BillingStatement;
 
     <?= $form->field($model, 'cost')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'employee_id')->dropDownList(
-        ArrayHelper::map( Employee::find()->all(), 'id', 'name' ),
-        [
-            'prompt'=>'Select Employee',
-            //'onchange'=>
-            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function
-            //  ( data ){
-            //      $( "select#model-contact" ).html( data );
-            //  });'
-        ]); ?>
+    <?= $form->field($model, 'employee_id')->textInput() ?>
 
-    <?= $form->field($model, 'walkin_id')->dropDownList(
-        ArrayHelper::map( Walkin::find()->all(), 'id', 'actual_time_reserve' ),
-        [
-            'prompt'=>'Select Walkin',
-            //'onchange'=>
-            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function
-            //  ( data ){
-            //      $( "select#model-contact" ).html( data );
-            //  });'
-        ]); ?>
+    <?= $form->field($model, 'customer_id')->textInput() ?>
 
-    <?= $form->field($model, 'customer_id')->dropDownList(
-        ArrayHelper::map( Customer::find()->all(), 'id', 'name' ),
-        [
-            'prompt'=>'Select Customer',
-            //'onchange'=>
-            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function
-            //  ( data ){
-            //      $( "select#model-contact" ).html( data );
-            //  });'
-        ]); ?>
-
-    <?= $form->field($model, 'billingstatement_id')->dropDownList(
-        ArrayHelper::map( BillingStatement::find()->all(), 'id', 'mode_of_payment' ),
-        [
-            'prompt'=>'Select BillingStatement',
-            //'onchange'=>
-            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function
-            //  ( data ){
-            //      $( "select#model-contact" ).html( data );
-            //  });'
-        ]); ?>
+    <?= $form->field($model, 'billingStatement_id')->textInput() ?>
 
     <?= $form->field($model, 'room_id')->textInput() ?>
 
