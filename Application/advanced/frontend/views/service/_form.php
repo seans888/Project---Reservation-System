393163fdc4a33f0998884eb0582c07ce7ec0353e
Maica Belchez <aciambelchez@gmail.com>
2016-09-03 21:02:36 +0800

    dropDownList - Service - employee ID

--- Application/advanced/frontend/views/service/_form.php
+++ Application/advanced/frontend/views/service/_form.php
@@ -2,6 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use common\models\Employee;
+use common\models\Customer;
+use common\models\BillingStatement;
+use common\models\Room;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Service */
@@ -16,7 +21,15 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'cost')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'employee_id')->textInput() ?>
+    <?= $form->field($model, 'employee_id')->dropDownList(
+		ArrayHelper::map( Employee::find()->all(), 'id', 'last_name' ),
+		[
+			'prompt'=>'Select Employee',
+			//'onchange'=>
+		    //	'$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+			//		$( "select#model-contact" ).html( data );
+			//	});'
+		]); ?>	
 
     <?= $form->field($model, 'customer_id')->textInput() ?>
 
