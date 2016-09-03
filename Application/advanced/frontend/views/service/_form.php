483a66cc85ec461b0e937f15264913057f0cc9dd
Kyle Lee <kylevlee@outlook.com>
2016-09-03 21:39:13 +0800

    Created dropdown list for customer_id, billingStatement_id, room_id, in service form

--- Application/advanced/frontend/views/service/_form.php
+++ Application/advanced/frontend/views/service/_form.php
@@ -31,11 +31,35 @@ use common\models\Room;
 			//	});'
 		]); ?>	
 
-    <?= $form->field($model, 'customer_id')->textInput() ?>
+    <?= $form->field($model, 'customer_id')->dropDownList( 
+    ArrayHelper::map( Customer::find()->all(), 'id', 'first_name' ),
+        [
+            'prompt'=>'Select Customer',
+            //'onchange'=>
+            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+            //      $( "select#model-contact" ).html( data );
+            //  });'
+        ]); ?>
 
-    <?= $form->field($model, 'billingStatement_id')->textInput() ?>
+    <?= $form->field($model, 'billingStatement_id')->dropDownList(
+    ArrayHelper::map( BillingStatement::find()->all(), 'id', 'mode_of_payment' ),
+        [
+            'prompt'=>'Select Mode of Payment',
+            //'onchange'=>
+            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+            //      $( "select#model-contact" ).html( data );
+            //  });'
+        ]); ?>
 
-    <?= $form->field($model, 'room_id')->textInput() ?>
+    <?= $form->field($model, 'room_id')->dropDownList(
+    ArrayHelper::map( Room::find()->all(), 'id', 'type' ),
+        [
+            'prompt'=>'Select Room Type',
+            //'onchange'=>
+            //  '$.post("index.php?r=work-orders/lists&id='.'" +$(this).val(), function( data ) {
+            //      $( "select#model-contact" ).html( data );
+            //  });'
+        ]); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
