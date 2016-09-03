2d6ff520fa57511ea74ad6a6b75460b5843cfa63
Kyle Lee <kvlee14@gmail.com>
2016-09-03 22:34:44 +0800

    edit the about page of the prototype

--- Application/advanced/frontend/views/site/about.php
+++ Application/advanced/frontend/views/site/about.php
@@ -6,66 +6,283 @@
 
 $this->title = 'About';
 
-?>
-<div class="site-about">
-    
-	<div class="jumbotron">
-	
-        <h1>About The Company</h1>
 
-        <img src = "http://www.villahotels.com/var/villa/storage/images/resorts/royal-island-resort-spa/3509-26-eng-GB/Royal-Island-Resort-Spa.jpg" style="width:100%">
-		
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-           dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-           ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-           fugiat nulla pariatur.</p>
-    </div>
-	
-	    <div class="body-content">
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Calimboracay</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="http://localhost/advanced/frontend/web">CALIMBORACAY</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active">
+                        <a href="http://localhost/advanced/frontend/web">Home</a>
+                    </li>
+                    <li class="active">
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                    
+                    
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="index.php?r=reservation">Reservation</a>
+                            </li>
+                            <li>
+                                <a href="index.php?r=customer">Customer</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
         <div class="row">
-            <div class="col-lg-4">
-			
-			<img src = "http://www-static.weddingbee.com/wp-content/uploads/2014/08/Hilton-Bora-Bora-Presidential-Suite-1024x748.jpg" style="width:100%">
-                <h2>History</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Read More &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-			
-			<img src = "http://www.terranea.com/assets/images/inset/careers.jpg" style="width:100%">
-                <h2>Team</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Read More &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-			
-			<img src = "http://kk.org/mt-files/ct2-mt/faq.jpg" style="width:100%">
-                <h2>FAQ</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Read More &raquo;</a></p>
+            <div class="col-lg-12">
+                <h1 class="page-header">About
+                   
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="http://localhost/advanced/frontend/web">Home</a>
+                    </li>
+                    <li class="active">About</li>
+                </ol>
             </div>
         </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive" src="http://conradhotels3.hilton.com/resources/media/ch/en_US/img/offers/information/image/100035919/CN_exteriorhotel1_750x450_FitToBoxSmallDimension_Center.jpg" alt="">
+            </div>
+            <div class="col-md-6">
+                <h2>About CALIMBORACAY</h2>
+                <p align ="justify">The Resort Reservation System, in cooperation with Mr. Alfredo Calimbo, focuses on the reservation process of the resort hotel. Its purpose is, first, to make the reservation process more convenient and faster for the sake of the customers, management and client. Second, it uses to help customers to reserve or to book without being hassled by a pushy sales representative. And lastly, it also uses to conduct transactions where only the client and management are the ones who monitor the cash flow.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Team</h2>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="http://www.thepanokrabi.com/wp-content/uploads/2015/07/750x450_facilities.jpg" alt="">
+                    <div class="caption">
+                        <h3>Anna Rey<br>
+                            <small>Front Desk</small>
+                        </h3>
+                        
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="http://www.itqlick.com/Blog/wp-content/uploads/2014/11/Fixed-Asset-Accounting-2-750x450.jpg" alt="">
+                    <div class="caption">
+                        <h3>Celine Bronx<br>
+                            <small>Accountant</small>
+                        </h3>
+                        
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="https://www.childrensmercy.org/library/uploadedImages/childrensmercy.org/Help_Our_Kids(1)/Volunteer_at_the_Hospital/4.3_guest_serv_role_image_750x450.jpg?n=8430" alt="">
+                    <div class="caption">
+                        <h3>Sky Walker<br>
+                            <small>Guest Service</small>
+                        </h3>
+                        
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="https://assets.rbl.ms/6477690/980x.jpg" alt="">
+                    <div class="caption">
+                        <h3>Daniel Roberts<br>
+                            <small>Lifeguard</small>
+                        </h3>
+                        
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="http://www.toppingbooks.co.uk/wp-content/uploads/Daniel-Doherty.jpg" alt="">
+                    <div class="caption">
+                        <h3>Simon Schloss<br>
+                            <small>Food Service</small>
+                        </h3>
+                       
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="http://www.jerseycleaninglady.com/images/recurring-jersey-cleaning-lady1.png" alt="">
+                    <div class="caption">
+                        <h3>Betty Sanchez<br>
+                            <small>Housekeeper Service</small>
+                        </h3>
+                        
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Our Customers -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Customers</h2>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://i1.wp.com/www.proformablog.com/wp-content/uploads/2014/08/Smart-Techs-Happy-customer_full.png" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="https://www.visioncritical.com/wp-content/uploads/2015/07/keeping-customers-happy-500x300.jpg" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://trustmotoring.com/wp-content/uploads/2016/08/52.jpg" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://chambermaster.blob.core.windows.net/images/customers/32/members/1227/photos/GALLERY_MAIN/customer_4.jpg" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="http://www.restaurantnews.com/wp-content/uploads/2014/01/How-to-Quickly-and-Quietly-Handle-Customer-Conflict.jpg" alt="">
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="https://life360-wordpress.s3.amazonaws.com/uploads/2012/11/shenna_c.jpg" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Calimboracay 2016</p>
+                </div>
+            </div>
+        </footer>
 
     </div>
-</div>
+    <!-- /.container -->
 
-    
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
 
-    
-</div>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
