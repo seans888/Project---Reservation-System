8abfaca9d13dc23b7e95e6b48f8601b993d194c6
Maica Belchez <aciambelchez@gmail.com>
2016-09-04 19:13:01 +0800

    edited code for Customer

--- Application/advanced/frontend/views/customer/create.php
+++ Application/advanced/frontend/views/customer/create.php
@@ -6,13 +6,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Customer */
 
-$this->title = 'Create Customer';
+$this->title = 'Book now!!!';
 $this->params['breadcrumbs'][] = ['label' => 'Customers', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="customer-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1><hr>
 
     <?= $this->render('_form', [
         'model' => $model,
