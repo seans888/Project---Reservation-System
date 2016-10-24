75cd0a8f0295a186f950ce674fecd459ec83e00a
Maica Belchez <aciambelchez@gmail.com>
2016-10-25 02:58:44 +0800

    link - about

--- Application/advanced/frontend/views/layouts/indexLayout.php
+++ Application/advanced/frontend/views/layouts/indexLayout.php
@@ -178,6 +178,8 @@ IndexAsset::register($this);
         <li><a href="#">Page 2</a></li>
         <li><a href="#">Page 3</a></li>
       </ul>
+	  <ul class="nav navbar-nav">
+		<li class="active"><a href="#">About</a><li>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
