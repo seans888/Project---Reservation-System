aab4c16c61a224f6ff1bcacc0821b251b8b2b6d0
Maica Belchez <aciambelchez@gmail.com>
2016-11-19 22:02:25 +0800

    Employee - model&crud generate

---
+++ Application/advanced/common/models/Employee.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "employee".
+ *
+ * @property integer $id
+ * @property string $last_name
+ * @property string $first_name
+ * @property string $middle_name
+ * @property string $contact_number
+ * @property string $address
+ * @property string $email
+ * @property string $hired_date
+ * @property string $rate
+ * @property string $type
+ * @property string $department
+ * @property string $position
+ *
+ * @property Billingstatement[] $billingstatements
+ * @property EmployeeAssistCustomer[] $employeeAssistCustomers
+ * @property Service[] $services
+ */
+class Employee extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'employee';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['hired_date'], 'safe'],
+            [['last_name', 'first_name', 'middle_name', 'rate'], 'string', 'max' => 20],
+            [['contact_number'], 'string', 'max' => 15],
+            [['address', 'email', 'department', 'position'], 'string', 'max' => 50],
+            [['type'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'last_name' => 'Last Name',
+            'first_name' => 'First Name',
+            'middle_name' => 'Middle Name',
+            'contact_number' => 'Contact Number',
+            'address' => 'Address',
+            'email' => 'Email',
+            'hired_date' => 'Hired Date',
+            'rate' => 'Rate',
+            'type' => 'Type',
+            'department' => 'Department',
+            'position' => 'Position',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBillingstatements()
+    {
+        return $this->hasMany(Billingstatement::className(), ['employee_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEmployeeAssistCustomers()
+    {
+        return $this->hasMany(EmployeeAssistCustomer::className(), ['employee_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getServices()
+    {
+        return $this->hasMany(Service::className(), ['employee_id' => 'id']);
+    }
+}
---
+++ Application/advanced/frontend/views/employee/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\EmployeeSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Employees';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="employee-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Employee', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'last_name',
+            'first_name',
+            'middle_name',
+            'contact_number',
+            // 'address',
+            // 'email:email',
+            // 'hired_date',
+            // 'rate',
+            // 'type',
+            // 'department',
+            // 'position',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
