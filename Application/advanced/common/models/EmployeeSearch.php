c650f7e4f2349eb7dbbd707847352c11fae1fe3a
Maica Belchez <aciambelchez@gmail.com>
2016-11-20 16:02:50 +0800

    Employee - updated model&crud generate (frontend)

---
+++ Application/advanced/common/models/EmployeeSearch.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Employee;
+
+/**
+ * EmployeeSearch represents the model behind the search form about `common\models\Employee`.
+ */
+class EmployeeSearch extends Employee
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['last_name', 'first_name', 'middle_name', 'contact_number', 'address', 'email', 'hired_date', 'rate', 'type', 'department', 'position'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Employee::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'hired_date' => $this->hired_date,
+        ]);
+
+        $query->andFilterWhere(['like', 'last_name', $this->last_name])
+            ->andFilterWhere(['like', 'first_name', $this->first_name])
+            ->andFilterWhere(['like', 'middle_name', $this->middle_name])
+            ->andFilterWhere(['like', 'contact_number', $this->contact_number])
+            ->andFilterWhere(['like', 'address', $this->address])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'rate', $this->rate])
+            ->andFilterWhere(['like', 'type', $this->type])
+            ->andFilterWhere(['like', 'department', $this->department])
+            ->andFilterWhere(['like', 'position', $this->position]);
+
+        return $dataProvider;
+    }
+}
