56149edcb058fde1cb3bb1e7ce1650b0eac8555a
Maica Belchez <aciambelchez@gmail.com>
2016-11-20 15:46:40 +0800

    Customer - updated model&crud generate (frontend)

---
+++ Application/advanced/frontend/views/customer/_search.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\CustomerSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="customer-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'last_name') ?>
+
+    <?= $form->field($model, 'first_name') ?>
+
+    <?= $form->field($model, 'middle_name') ?>
+
+    <?= $form->field($model, 'contact_number') ?>
+
+    <?php // echo $form->field($model, 'address') ?>
+
+    <?php // echo $form->field($model, 'email') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
