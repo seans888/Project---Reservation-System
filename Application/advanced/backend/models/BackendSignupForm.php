c5842ef6072e95e19888a3d079a1e77ea88cb6b0
Maica Belchez <aciambelchez@gmail.com>
2016-11-21 22:07:57 +0800

    edited BackSignupForm.php

--- Application/advanced/backend/models/BackendSignupForm.php
+++ Application/advanced/backend/models/BackendSignupForm.php
@@ -1,15 +1,15 @@
 <?php
-namespace frontend\models;
+namespace backend\models;
 
 use yii\base\Model;
-use common\models\User;
+use common\models\Admin;
 
 /**
  * Signup form
  */
-class SignupForm extends Model
+class BackendSignupForm extends Model
 {
-    public $username;
+    public $admin;
     public $email;
     public $password;
 
@@ -19,17 +19,17 @@ class SignupForm extends Model
      */
     public function rules()
     {
-        return [
-            ['username', 'trim'],
-            ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
-            ['username', 'string', 'min' => 2, 'max' => 255],
+       return [
+            ['admin', 'filter', 'filter' => 'trim'],
+            ['admin', 'required'],
+            ['admin', 'unique', 'targetClass' => '\common\models\Admin', 'message' => 'This username has already been taken.'],
+            ['admin', 'string', 'min' => 2, 'max' => 255],
 
-            ['email', 'trim'],
+            ['email', 'filter', 'filter' => 'trim'],
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            ['email', 'unique', 'targetClass' => '\common\models\Admin', 'message' => 'This email address has already been taken.'],
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
@@ -47,8 +47,8 @@ class SignupForm extends Model
             return null;
         }
         
-        $user = new User();
-        $user->username = $this->username;
+        $user = new Admin();
+        $user->admin = $this->admin;
         $user->email = $this->email;
         $user->setPassword($this->password);
         $user->generateAuthKey();
