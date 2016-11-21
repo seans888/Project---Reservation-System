69c0135b211b93ae5af2cfd5787d702fc6877138
Maica Belchez <aciambelchez@gmail.com>
2016-11-21 20:07:36 +0800

    generate Employee (frontend)

---
+++ Application/advanced/frontend/views/employee/_search.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\EmployeeSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="employee-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'last_name') ?>
+
+    <?= $form->field($model, 'first_name') ?>
+
+    <?= $form->field($model, 'middle_name') ?>
+
+    <?= $form->field($model, 'contact_number') ?>
+
+    <?php // echo $form->field($model, 'address') ?>
+
+    <?php // echo $form->field($model, 'email') ?>
+
+    <?php // echo $form->field($model, 'hired_date') ?>
+
+    <?php // echo $form->field($model, 'rate') ?>
+
+    <?php // echo $form->field($model, 'type') ?>
+
+    <?php // echo $form->field($model, 'department') ?>
+
+    <?php // echo $form->field($model, 'position') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
