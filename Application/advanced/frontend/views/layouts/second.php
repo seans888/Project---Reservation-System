be17a3b410a282b29b31155a5d4483af7cf9be84
Kyle Lee <kylevlee@outlook.com>
2016-11-27 02:29:18 +0800

    runtime and new design

--- Application/advanced/frontend/views/layouts/second.php
+++ Application/advanced/frontend/views/layouts/second.php
@@ -1,9 +1,18 @@
 <?php
 
-/* @var $this yii\web\View */
+/* @var $this \yii\web\View */
+/* @var $content string */
 
-$this->title = 'My Yii Application';
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\AppAsset;
+use common\widgets\Alert;
+
+AppAsset::register($this);
 ?>
+<?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html>
 <head>
