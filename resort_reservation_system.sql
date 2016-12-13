a280a9114c7aca49fb510aeb2c9aa0ab45f8a235
Maica Belchez <aciambelchez@gmail.com>
2016-12-13 21:41:49 +0800

    edit

--- resort_reservation_system.sql
+++ resort_reservation_system.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `resort_reservation_system`
+-- Database: `resort_reservation_system2`
 --
 
 -- --------------------------------------------------------
